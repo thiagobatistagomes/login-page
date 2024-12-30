ea41a3af3692dc0aaf314eec1b4ab4e8e0f755bd
Thiago <thiagobg1811@gmail.com>
2024-12-30 16:27:30 -0300

    final changes

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { LoginResponse } from '../types/LoginResponse.type';
+import { LoginResponse } from '../types/loginResponse.type';
 import { tap } from 'rxjs';
 
 @Injectable({
@@ -8,10 +8,21 @@ import { tap } from 'rxjs';
 })
 export class LoginService {
 
+  apiUrl: string = "http://localhost:8080/auth"
+
   constructor(private httpClient: HttpClient) { }
 
-  login(name: string, password: string){
-    return this.httpClient.post<LoginResponse>("/login",{ name, password }).pipe(
+  login(email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+      tap((value) => {
+        sessionStorage.setItem("auth-token", value.token)
+        sessionStorage.setItem("username", value.name)
+      })
+    )
+  }
+
+  signup(name:string, email: string, password: string){
+    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
